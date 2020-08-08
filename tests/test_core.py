445e69d3bb78d26377ec73d221cbcff55a6a3845
Adrian Zuber <xadrianzetx@gmail.com>
2020-08-08 20:03:14 +0200

    add tests for ID3 entropy

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from msitrees._core import gini_impurity
+from msitrees._core import gini_impurity, entropy
 
 
 class TestGiniImpurity(unittest.TestCase):
@@ -64,6 +64,68 @@ class TestGiniImpurity(unittest.TestCase):
         max_impurity = 1 - (1 / arr.shape[0])
         gini = gini_impurity(arr)
         self.assertAlmostEqual(gini, max_impurity)
+
+
+class TestEntropy(unittest.TestCase):
+
+    def test_input_type_list(self):
+        try:
+            entropy([0, 0])
+
+        except TypeError:
+            self.fail('Exception on allowed input type - list')
+
+    def test_input_type_tuple(self):
+        try:
+            entropy((0, 0))
+
+        except TypeError:
+            self.fail('Exception on allowed input type - tuple')
+
+    def test_input_type_numpy(self):
+        try:
+            entropy(np.array([0, 0]))
+
+        except TypeError:
+            self.fail('Exception on allowed input type - np.ndarray')
+
+    def test_input_int(self):
+        with self.assertRaises(ValueError):
+            entropy(0)
+
+    def test_input_other(self):
+        with self.assertRaises(TypeError):
+            entropy('foo')
+
+        with self.assertRaises(TypeError):
+            entropy({'foo': 1})
+
+    def test_input_wrong_shape(self):
+        with self.assertRaises(ValueError):
+            entropy(np.array([[1, 0], [1, 0]]))
+
+    def test_input_empty_list(self):
+        with self.assertRaises(ValueError):
+            entropy([])
+
+    def test_input_empty_array(self):
+        with self.assertRaises(ValueError):
+            entropy(np.array([]))
+
+    def test_binary_max_impurity(self):
+        arr = np.array([1, 0, 1, 0])
+        hs = entropy(arr)
+        self.assertAlmostEqual(hs, 1.)
+
+    def test_binary_min_impurity(self):
+        arr = np.array([0, 0, 0, 0])
+        hs = entropy(arr)
+        self.assertAlmostEqual(hs, 0.)
+
+    def test_multiclass_max_impurity(self):
+        arr = np.array([1, 2, 3, 4])
+        hs = entropy(arr)
+        self.assertAlmostEqual(hs, 2.)
 
 
 if __name__ == "__main__":
