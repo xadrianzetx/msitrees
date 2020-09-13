6f5f2387b4ba560687fb687a9e912c47016837ae
Adrian Zuber <xadrianzetx@gmail.com>
2020-09-13 18:43:41 +0200

    inherit from base

--- msitrees/ensemble.py
+++ msitrees/ensemble.py
@@ -3,10 +3,11 @@ import numpy as np
 import pandas as pd
 from typing import Union, Optional
 
+from msitrees._base import MSIBaseClassifier
 from msitrees.tree import MSIDecisionTreeClassifier
 
 
-class MSIRandomForestClassifier:
+class MSIRandomForestClassifier(MSIBaseClassifier):
 
     def __init__(self, n_estimators: int = 100,
                  bagging_fraction: float = 1.0,
@@ -14,11 +15,8 @@ class MSIRandomForestClassifier:
                  n_jobs: int = -1,
                  random_state: Optional[int] = None,
                  **kwargs):
+        super().__init__()
         self._estimators = []
-        self._fitted = False
-        self._shape = None
-        self._ncls = None
-        self._ndim = None
         self._importances = None
         self._n_estimators = n_estimators
         self._frac_bagging = bagging_fraction
@@ -46,6 +44,9 @@ class MSIRandomForestClassifier:
         pass
 
     def predict_log_proba(self):
+        pass
+
+    def score(self):
         pass
 
     def get_params(self):
