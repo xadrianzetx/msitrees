bc31731d8082d229272c5fd4ab0b060aac1d1bf3
Adrian Zuber <xadrianzetx@gmail.com>
2020-09-14 20:51:32 +0200

    implemented max_features mode selection

--- msitrees/ensemble.py
+++ msitrees/ensemble.py
@@ -11,7 +11,7 @@ class MSIRandomForestClassifier(MSIBaseClassifier):
 
     def __init__(self, n_estimators: int = 100,
                  bootstrap: bool = True,
-                 feature_fraction: float = 1.0,
+                 max_features: Union[int, float, str] = 'auto',
                  n_jobs: int = -1,
                  random_state: Optional[int] = None,
                  **kwargs):
@@ -20,7 +20,7 @@ class MSIRandomForestClassifier(MSIBaseClassifier):
         self._importances = None
         self._n_estimators = n_estimators
         self._bootstrap = bootstrap
-        self._frac_feats = feature_fraction
+        self._max_features = max_features
         self._n_jobs = n_jobs
         self._random_state = random_state
 
@@ -30,6 +30,44 @@ class MSIRandomForestClassifier(MSIBaseClassifier):
     @property
     def feature_importances_(self):
         pass
+
+    def _num_features_per_tree(self) -> Optional[int]:
+        """Resolves number of features to use
+        when fitting estimator.
+        """
+        if self._max_features is None:
+            # no feature subsampling
+            return self._shape[1]
+
+        if isinstance(self._max_features, int):
+            # user specified number of features
+            # to use directly
+            if self._max_features > self._shape[1]:
+                raise ValueError('max_features is greater than '
+                                 'overall number of features in dataset')
+            return self._max_features
+
+        if isinstance(self._max_features, float):
+            # number of features to use specified
+            # as fraction of overall shape
+            if self._max_features > 1.0:
+                raise ValueError('Feature fraction used to build '
+                                 'a tree cannot be greater than 1')
+            return round(self._max_features * self._shape[1])
+
+        modes = {
+            **dict.fromkeys(['auto', 'sqrt'], int(np.sqrt(self._shape[1]))),
+            'log2': np.log2(self._shape[1])
+        }
+
+        try:
+            num_features = modes[self._max_features]
+            return num_features
+
+        except KeyError:
+            msg = 'Allowed feature fraction modes are {} got {}.'
+            allowed = list(modes.keys())
+            raise ValueError(msg.format(allowed, self._max_features))
 
     def _add_estimator(self, x: np.ndarray,
                        y: np.ndarray) -> MSIDecisionTreeClassifier:
