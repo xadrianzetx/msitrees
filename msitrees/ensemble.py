7a03fbdd34a86dc469fd1edf6c4157131d4e9129
Adrian Zuber <xadrianzetx@gmail.com>
2020-09-14 20:15:29 +0200

    implemented add_estimator

--- msitrees/ensemble.py
+++ msitrees/ensemble.py
@@ -10,7 +10,7 @@ from msitrees.tree import MSIDecisionTreeClassifier
 class MSIRandomForestClassifier(MSIBaseClassifier):
 
     def __init__(self, n_estimators: int = 100,
-                 bagging_fraction: float = 1.0,
+                 bootstrap: bool = True,
                  feature_fraction: float = 1.0,
                  n_jobs: int = -1,
                  random_state: Optional[int] = None,
@@ -19,7 +19,7 @@ class MSIRandomForestClassifier(MSIBaseClassifier):
         self._estimators = []
         self._importances = None
         self._n_estimators = n_estimators
-        self._frac_bagging = bagging_fraction
+        self._bootstrap = bootstrap
         self._frac_feats = feature_fraction
         self._n_jobs = n_jobs
         self._random_state = random_state
@@ -31,8 +31,27 @@ class MSIRandomForestClassifier(MSIBaseClassifier):
     def feature_importances_(self):
         pass
 
-    def _add_estimator(self):
-        pass
+    def _add_estimator(self, x: np.ndarray,
+                       y: np.ndarray) -> MSIDecisionTreeClassifier:
+        """Builds new estimator for ensemble
+
+        TODO feature fraction should + should track which
+        features went to which estimator if feature fraction
+        is used
+        """
+        if self._bootstrap:
+            # sample dataset with replacement
+            # before fitting new estimator
+            indices = np.arange(start=0, stop=self._shape[0])
+            bstrap_idx = np.random.choice(indices, size=self._shape[0])
+            x = x[bstrap_idx, :]
+            y = y[bstrap_idx]
+
+        # fit decision tree classifier
+        clf = MSIDecisionTreeClassifier()
+        clf._internal_fit(x, y, n_class=self._ncls)
+
+        return clf
 
     def fit(self):
         pass
